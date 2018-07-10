302d1267e7b2e23486b07f0421ab9bdc4254806a
Ryan M. Waite <ryanwaite28@gmail.com>
2018-07-10 16:15:40 -0400

    updated: 7/10/2018 - 4:15 PM EST

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
-(function(){
+$(document).ready(function(){
 
   const MVC = function MVC() {
     let self = this;
 
     self.name = ko.observable('Ryan M. Waite');
+
+    self.card_links = [
+
+    ];
+
+    self.slack_images = [
+      { title: "", alt: "", src: "img/slack-moments/sm-1.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-2.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-3.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-4.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-5.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-6.JPG" },
+      { title: "", alt: "", src: "img/slack-moments/sm-7.JPG" },
+    ];
   }
 
   ko.applyBindings(new MVC());
 
-})()
+  //
+
+  // $('.fade').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   fade: true,
+  //   cssEase: 'linear'
+  // });
+
+  $('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+});
